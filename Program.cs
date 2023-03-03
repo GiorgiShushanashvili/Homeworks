93dc376de8d5d6ba7d85cbbba825e9402345d6ab
GiorgiShushanashvili <shushanashvili20@gmail.com>
2023-03-03 23:29:53 +0400

    very first change

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("Enter your firstname");
+var name = Console.ReadLine();
+Console.WriteLine("Enter your lastname");
+var lastname = Console.ReadLine();
+Console.WriteLine($"my name is {name} and my lastname is {lastname}");
+Console.ReadLine();
